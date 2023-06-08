140d8dcc3f961073b9d65e2ea61adc2d34493bb0
GridexX <arsene582@gmail.com>
2023-06-08 17:12:09 +0200

    feat: add error on retrieve
    Signed-off-by: GridexX <arsene582@gmail.com>

--- web_app/index.js
+++ web_app/index.js
@@ -5,28 +5,47 @@ const MS_URL = process.env.MS_URL || "http://localhost:3001";
 const axios = require("axios");
 
 const getCounter = async () => {
-  return await axios.get(MS_URL + "/count").then(response => response.data.count);
+  return await axios.get(MS_URL + "/api/count").then(response => response.data.count);
 }
 
 const postCounter = async () => {
-  return await axios.post(MS_URL + "/count").then(response => response.data.count);
+  return await axios.post(MS_URL + "/api/count").then(response => response.data.count);
 
 }
 
 app.get('/', async (req, res) => {
   res.set('Content-Type', 'text/html');
-  const counter = await getCounter();
-  res.send(`<h1>Counter</h1>
+  try {
+
+    const counter = await getCounter();
+    res.send(`<h1>Counter</h1>
     <p>${counter}</p>
     <form action="/increment" method="post">
-      <button type="submit">Add</button>
+    <button type="submit">Add</button>
     </form>
-  `);
+    `);
+  } catch (e) {
+    res.send(
+      `
+      <h1>Counter</h1>
+      <p>Error fetching the API: ${JSON.stringify(e)}</p>
+      `
+    )
+  }
 });
 
 app.post('/increment', async (req, res) => {
-  await postCounter();
-  res.redirect('/');
+  try {
+    await postCounter();
+    res.redirect('/');
+  } catch (e) {
+    res.send(
+      `
+      <h1>Counter</h1>
+      <p>Error posting the API: ${JSON.stringify(e)}</p>
+      `
+    )
+  }
 });
 
 app.get('/ready', async (req, res) => {
@@ -36,17 +55,12 @@ app.get('/ready', async (req, res) => {
   })
 })
 
-// app.post('/count', async (req, res) => {
-//   req.
-// })
-
 app.get('/health', async (req, res) => {
   res.set('Content-Type', 'application/json');
   res.send({
     health: "OK"
   })
 })
-
 
 
 app.listen(port, () => {
