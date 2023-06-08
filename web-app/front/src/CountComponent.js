e70f2ddceb4613bc03bb4358a15fd318286d41a5
Alexis Langlet <alexis.langlet@etu.umontpellier.fr>
2023-06-08 12:56:50 +0200

    feat: use env var

--- web-app/front/src/CountComponent.js
+++ web-app/front/src/CountComponent.js
@@ -1,38 +1,38 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 const CountComponent = () => {
-    const countUrl = 'http://localhost:3001/count';
+  const countUrl = process.env.REACT_APP_COUNT_URI;
   const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    fetchData();
-  }, []);
 
   const fetchData = async () => {
     try {
       const response = await fetch(countUrl);
       if (!response.ok) {
-        throw new Error('Network response was not ok');
+        throw new Error("Network response was not ok");
       }
       const data = await response.json();
       setCount(data.count);
     } catch (error) {
-      console.error('Error:', error);
+      console.error("Error:", error);
     }
   };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   const handleIncrement = async () => {
     try {
       const response = await fetch(countUrl, {
-        method: 'POST',
+        method: "POST",
       });
       if (!response.ok) {
-        throw new Error('Network response was not ok');
+        throw new Error("Network response was not ok");
       }
       // Update the count after successful POST request
       setCount((await response.json()).count);
     } catch (error) {
-      console.error('Error:', error);
+      console.error("Error:", error);
     }
   };
 
