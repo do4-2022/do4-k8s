dd2a371a3ceabb5ac7e47ae32d8548f07a1366cd
WoodenMaiden <yann.pomie@laposte.net>
2023-06-08 16:31:03 +0200

    feat: basic ressources
    Signed-off-by: WoodenMaiden <yann.pomie@laposte.net>

--- CounterService/src/main/java/fr/polytech/yannpomie/counterservice/repository/DatabaseRepository.java
+++ CounterService/src/main/java/fr/polytech/yannpomie/counterservice/repository/DatabaseRepository.java
@@ -2,6 +2,5 @@ package fr.polytech.yannpomie.counterservice.repository;
 
 import fr.polytech.yannpomie.counterservice.domain.Counter;
 import org.springframework.data.jpa.repository.JpaRepository;
-import reactor.core.publisher.Mono;
 
 public interface DatabaseRepository extends JpaRepository<Counter, String> {}
