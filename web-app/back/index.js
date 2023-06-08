449b87ac7586ac37bdc52caca92969aea68b4166
Alexis Langlet <alexis.langlet@etu.umontpellier.fr>
2023-06-08 12:30:22 +0200

    feat: switch to express

---
+++ web-app/back/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/count", (req, res) => {
+  res.send(1);
+});
+
+app.post("/count", (req, res) => {
+  res.send(2);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
