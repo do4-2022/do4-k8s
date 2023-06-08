c103b53be5a86e5348fac0ce5da510e729175107
WoodenMaiden <yann.pomie@laposte.net>
2023-06-08 12:21:53 +0200

    chore: base architecture
    Signed-off-by: WoodenMaiden <yann.pomie@laposte.net>

---
+++ CounterService/src/test/java/fr/polytech/yannpomie/counterservice/CounterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.polytech.yannpomie.counterservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CounterServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
